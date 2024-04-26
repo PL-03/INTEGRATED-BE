d259a192bcff9948bd875bf09088b7ec929256fa
Threepoom <threepoom.ngam@kmutt.ac.th>
2024-04-26 13:12:47 +0700

    Add CrossOrigin

--- src/main/java/com/pl03/kanban/KanbanApplication.java
+++ src/main/java/com/pl03/kanban/KanbanApplication.java
@@ -2,10 +2,15 @@ package com.pl03.kanban;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+//@GetMapping("/v1/task")
 @SpringBootApplication
+@CrossOrigin (origins = "http://localhost:5173")
 public class KanbanApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KanbanApplication.class, args);
 	}
--- src/main/java/com/pl03/kanban/controllers/TaskController.java
+++ src/main/java/com/pl03/kanban/controllers/TaskController.java
@@ -5,12 +5,14 @@ import com.pl03.kanban.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/itb-kk/v1")
 public class TaskController {
