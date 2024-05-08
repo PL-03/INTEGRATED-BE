6632e0029de3bc565af61c6274476a3983630ea6
mattiiee <matboonmalert8@gmail.com>
2024-05-08 15:24:35 +0700

    trim status name and description

--- src/main/java/com/pl03/kanban/dtos/StatusDto.java
+++ src/main/java/com/pl03/kanban/dtos/StatusDto.java
@@ -7,4 +7,12 @@ public class StatusDto {
     private int id;
     private String name;
     private String description;
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
 }
