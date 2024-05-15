7e5796bd82d7fc7c7b21460270da53a9d206ac25
Pimpajee <pimpajee.seth@mail.kmutt.ac.th>
2024-05-15 16:02:25 +0700

    edit controller and application

--- src/main/java/com/pl03/kanban/controllers/TaskController.java
+++ src/main/java/com/pl03/kanban/controllers/TaskController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins =  {"http://localhost:5173","http://ip23pl3.sit.kmutt.ac.th}","http://ip23pl3.sit.kmutt.ac.th"})
 @RestController
 @RequestMapping
 public class TaskController {
