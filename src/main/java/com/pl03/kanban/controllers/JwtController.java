f21215041bf7faa8262c451a3bd35dd15bab1082
mattiiee <matboonmalert8@gmail.com>
2024-08-23 00:34:07 +0700

    add cross origin

--- src/main/java/com/pl03/kanban/controllers/JwtController.java
+++ src/main/java/com/pl03/kanban/controllers/JwtController.java
@@ -3,12 +3,17 @@ package com.pl03.kanban.controllers;
 
 import com.pl03.kanban.configs.JwtToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-
+@CrossOrigin(origins = {"http://localhost:5173",
+        "http://intproj23.sit.kmutt.ac.th",
+        "http://intproj23.sit.kmutt.ac.th/pl3",
+        "http://intproj23.sit.kmutt.ac.th/pl3/status",
+        "http://ip23pl3.sit.kmutt.ac.th"})
 @RestController
 public class JwtController {
 
